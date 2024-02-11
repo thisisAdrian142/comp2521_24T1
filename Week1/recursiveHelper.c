a74835cf595716daeb362857781ed96e867c160e
thisisAdrian142 <EmailAdrian.png@gmail.com>
2024-02-11 17:05:46 +1100

    recursiveHelper: correct

--- Week1/recursiveHelper.c
+++ Week1/recursiveHelper.c
@@ -7,7 +7,24 @@ struct list{
     struct node*head;
 };
 
-void listAppend(struct list*list, int value);
+void listAppend(struct list*list, int value) {
+    list->head=doListAppend(list->head,value);
+}
+
+struct node*doListAppend(struct node*node, int value){
+    if(node==NULL){
+        return newNode(value);
+    }else{
+        node->next=doListAppend(node->next, value);
+        return node;
+    }
+} 
 
 //* side notes:
-// why do we have to use so many structs??
+// we cant recurse this "listAppend" function
+// since the recursive function needs to take in
+// a struct node pointer
+
+//* solution -> use a recursive helper function
+
+// ADDING NEW ELEMENTS TO THE END OF THE LIST
