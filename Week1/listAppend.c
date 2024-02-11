97cf6db34bcacbaffd33637e4e0ebe8fd18d1722
thisisAdrian142 <EmailAdrian.png@gmail.com>
2024-02-11 16:51:55 +1100

    listAppend: a more optimal code :)

--- Week1/listAppend.c
+++ Week1/listAppend.c
@@ -1,14 +1,9 @@
 struct node*listAppend(struct node*list, int value) {
-    if (list==NULL){
+    if(list==NULL){
         return newNode(value);
-    } else if (list->next==NULL) {
-        list->next=newNode(value);
-        return list;
     } else {
-        listAppend(list->next,value);
+        list->next=listAppend(list->next,value);
         return list;
     }
 }
 
-//cons: repeats the call to newNode 
-//cons: repeats rturn list;
